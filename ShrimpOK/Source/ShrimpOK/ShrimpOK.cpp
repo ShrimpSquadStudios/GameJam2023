38aff398653c3ee038329ece1dd41c8d4dc7e1f6
Megan Sumner <msumner@ualberta.ca>
2023-02-04 11:29:50 -0700

    Adding ShrimpOK project to git

---
+++ ShrimpOK/Source/ShrimpOK/ShrimpOK.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShrimpOK.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShrimpOK, "ShrimpOK" );
+
+DEFINE_LOG_CATEGORY(LogShrimpOK)
+ 
